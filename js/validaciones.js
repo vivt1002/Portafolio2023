ab60659c129c9d7201e004cfb6a957cb0e6b2ff2
Viviana <tapiaviviana2012@gmail.com>
2023-04-10 16:57:39 -0300

    link redes sociales

---
+++ js/validaciones.js
@@ -0,0 +1,38 @@
+export function valida(input) {
+    const tipoDeInput = input.dataset.tipo;
+    if (validadores[tipoDeInput]) {
+        validadores[tipoDeInput](input);
+    }
+
+
+    if (input.validity.valid) {
+        input.parentElement.classList.remove("input-container--invalid");
+        input.parentElement.querySelector(".input-message-error").innerHTML = "";
+    } else {
+        input.parentElement.classList.add("input-container--invalid");
+        input.parentElement.querySelector(".input-message-error").innerHTML = mostrarMensajeDeError(tipoDeInput, input);
+    }
+}
+
+const tipoDeErrores = [
+    "valueMissing",
+    "typeMismatch",
+    "patternMismatch",
+    "customError",
+];
+
+const mensajesDeError = {
+    nombre: {
+        valueMissing: "El campo nombre no puede estar vacio",
+    },
+    email: {
+        valueMissing: "El campo correo no puede estar vacio",
+        typeMismatch: "El correo no es valido",
+    },
+
+    numero: {
+        valueMissing: "Este campo no puede estar vacío",
+        patternMismatch: "El formato requerido es XXXXXXXXXX",
+    },
+    
+};
